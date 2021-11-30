89b958fdaef825bdf59ba54606ac24ff24e81d0f
Christian Ekrem <cekrem@pm.me>
2021-11-30 21:52:37 +0100

    add content

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import {useState} from "react";
 const slots = [
     'Massasje når Levi sovner i kveld?',
     'Jeg rydder opp og shiner kjøkken etter lunsj!',
-    'Du velger film i kveld, baby <3',
     'Jeg lager ekstra god middag!',
+    'Jeg ryddeg og fikset mens du sov!',
+    'Du velger film i kveld, baby <3',
+    'Fotmassasje når som helst i dag!',
+    'Jeg kan hente også, så får du litt ekstra tid (kan godt lage middag etter det, hvis du vil).',
+    'Velg en sang, så lærer jeg den på gitar og framfører når den er klar!',
+    'Jeg gjør hva som helst for deg, uten unntak!',
+    'Deleger noe kjedelig til meg som du hadde tenkt å gjøre – jeg tar det!',
+    'Jeg lager Pad Thai i kveld!',
+    'Massasje any way you like!',
 ]
 
 const slotsWithDefault = Array(24).fill('Tom luke?!').map((def, i) => slots[i] || def)
