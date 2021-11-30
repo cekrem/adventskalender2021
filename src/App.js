c3a9584f8e34706d02e9e126834fdd308ff3d4d6
Christian Ekrem <cekrem@pm.me>
2021-11-30 21:19:40 +0100

    add no-cheat

--- src/App.js
+++ src/App.js
@@ -10,22 +10,27 @@ const slots = [
 
 const slotsWithDefault = Array(24).fill('Tom luke?!').map((def, i) => slots[i] || def)
 
-const snowflakes = Array(20).fill().map(() => ({
+const snowflakes = Array(20).fill(0).map(() => ({
     size: `${Math.random() * 8}rem`,
     delay: `${Math.random() * 12}s`,
     position: `${Math.random() * 100}%`,
     opacity: Math.random() * 0.5,
 }))
 
+const now = new Date()
+const today = now.getMonth() === 11 ? now.getDate() : 0
+
+console.log(today)
 const Slot = ({content, index}) => {
     const [open, setOpen] = useState(false)
+    const slotDay = index + 1
 
     return <div onClick={setOpen} className={open ? 'slot open' : 'slot'}>
         <div className="front">
-            <h1>{index + 1}</h1>
+            <h1>{slotDay}</h1>
         </div>
         <div className="back">
-            <p>{content}</p>
+            <p>{slotDay > today ? 'Ikke juks!' : content}</p>
         </div>
     </div>
 }
@@ -38,10 +43,10 @@ const App = () =>
                     Birgittes advendskalender 2021
                 </h1>
             </header>
-            {slotsWithDefault.map((content, index) => Slot({content, index}))}
-        </div>
+            {slotsWithDefault.map((content, index) => <Slot content={content} index={index} key={index}/>)}
+        < /div>
         <div className="snowflakes">
-            {snowflakes.map(({size, delay, position, opacity}) => <div style={{
+            {snowflakes.map(({size, delay, position, opacity}) => <div key={position} style={{
                 fontSize: size,
                 left: position,
                 animationDelay: delay,
