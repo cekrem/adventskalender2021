8948a0f849f440329b4e4eba75ae23962fbdbbfd
Christian Ekrem <cekrem@pm.me>
2021-12-05 09:05:24 +0100

    change order of slots (overslept, haha)

--- src/slots.js
+++ src/slots.js
@@ -3,13 +3,13 @@ const slots = [
   "Jeg rydder opp og shiner kjøkken etter lunsj!",
   "Jeg lager ekstra god middag!",
   "Fotmassasje når som helst i dag!",
-  "Jeg ryddet og fikset mens du sov!",
+  "Velg en sang, så lærer jeg den på gitar og framfører når den er klar!",
   "Du velger film i kveld, baby <3",
   "Jeg kan hente også, så får du litt ekstra tid (kan godt lage middag etter det, hvis du vil).",
-  "Velg en sang, så lærer jeg den på gitar og framfører når den er klar!",
   "Jeg gjør hva som helst for deg, uten unntak!",
   "Deleger noe kjedelig til meg som du hadde tenkt å gjøre – jeg tar det!",
   "Jeg lager Pad Thai i kveld!",
+  "Jeg ryddet og fikset mens du sov!",
   "Massasje any way you like!",
 ];
 
