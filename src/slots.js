581c0f25266d6c5d299950802ca4027ad5c9627d
Christian Ekrem <cekrem@pm.me>
2021-12-16 09:00:47 +0100

    add content

--- src/slots.js
+++ src/slots.js
@@ -12,6 +12,11 @@ const slots = [
   "Jeg lager Pad Thai i kveld!",
   "Jeg ryddet og fikset mens du sov!",
   "Jeg kan hente også, så får du litt ekstra tid (kan godt lage middag etter det, hvis du vil).",
+  "Jeg kjører et flyttelass for oss på frikvelden min :)",
+  "Jeg tar kidsa og lager middag",
+  "Massasje på benk (ikke valgfritt)",
+  "Jeg står opp med jentene i morgen (hvis nødvendig), og tar Levi med meg hvis han våkner tidlig. Du kan sove til 9:30!",
+  "Jeg lager middag og tar oppvasken i kveld mens du legger Levi!",
 ];
 
 export const slotsWithDefault = Array(24)
